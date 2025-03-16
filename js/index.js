96249fcd6d6458b2546964a1cf85978197984b87
dgusfr <diegogustavo.fr@gmail.com>
2025-03-15 21:30:02 -0300

    feat: enhance header menu functionality and improve layout responsiveness

--- js/index.js
+++ js/index.js
@@ -24,13 +24,26 @@ initScrollSuave();
 document.addEventListener('DOMContentLoaded', function () {
   const menuHamburguer = document.getElementById('menu-hamburguer');
   const navLinks = document.getElementById('nav-links');
+  const mainContent = document.getElementById('main-content');
+  const footer = document.getElementById('footer');
 
   menuHamburguer.addEventListener('click', function () {
       navLinks.classList.toggle('open');
       const isOpen = navLinks.classList.contains('open');
       menuHamburguer.setAttribute('aria-expanded', isOpen);
+
+      if (isOpen) {
+          mainContent.style.marginTop = `${navLinks.scrollHeight}px`;
+      } else {
+          mainContent.style.marginTop = '0';
+      }
+  });
+
+  footer.addEventListener('click', function () {
+      window.scrollBy({ top: -100, left: 0, behavior: 'smooth' });
   });
 });
+
 
 // Atualizar o ano no footer
 document.addEventListener('DOMContentLoaded', function () {
